ff423bfc8e5c38b056ea9aa60273f002e7985300
shakir emstell <shakir.m@emstell.com>
2019-12-17 11:55:46 +0530

    create user class

---
+++ app/src/main/java/shakir/kadakkadan/databinding/example/User.kt
@@ -0,0 +1,9 @@
+package shakir.kadakkadan.databinding.example
+
+data class User(
+    val name: String,
+    val age: String,
+    val userName: String,
+    val password: String,
+    val email: String
+)
