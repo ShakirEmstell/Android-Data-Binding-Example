1315653ff4c1eba12a98005c92809ddae1b6e62f
shakir emstell <shakir.m@emstell.com>
2019-12-17 12:04:02 +0530

    bind user in MainActivity

--- app/src/main/java/shakir/kadakkadan/databinding/example/MainActivity.kt
+++ app/src/main/java/shakir/kadakkadan/databinding/example/MainActivity.kt
@@ -1,13 +1,17 @@
 package shakir.kadakkadan.databinding.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import shakir.kadakkadan.databinding.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = getUserApi()
     }
 
 
